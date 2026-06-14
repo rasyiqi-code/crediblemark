b1724b9bc27ef1ad9e6d1c1a98cf45373ceb0549
rasyiqi-code <support@crediblemark.com>
2026-06-14 15:54:04 +0700

    refactor: hapus file product-generator.ts beserta export-nya karena tidak dibutuhkan lagi

--- app/genkit/index.ts
+++ app/genkit/index.ts
@@ -5,4 +5,3 @@ export * from './flows/estimator';
 export * from './flows/service-content-generator';
 export * from './flows/service-pricing-generator';
 export * from './flows/service-addons-generator';
-export * from './flows/product-generator';
